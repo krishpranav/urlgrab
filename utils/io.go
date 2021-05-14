35b1afe5faaedc9c1fbd1215f51fb82540081886
krishpranav <krisna.pranav@gmail.com>
2021-05-14 03:14:32 -0400

    update readlines function

--- utils/io.go
+++ utils/io.go
@@ -54,3 +54,18 @@ func WriteLines(outputPath string, data []string) {
 		return
 	}
 }
+
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
